6b3a8ce131c128dfa16a5ce63d1f452170eab810
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-11-12 20:06:37 +0300

    Create app configurstions.

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
@@ -6,3 +7,13 @@ from flask_login import LoginManager
 def create_app(config_name):
 
     app = Flask(__name__)
+
+    #creating the app configurations
+    app.config.from_object(config_options[config_name])
+    config_options[config_name]
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
