a41b016b238044a60e33a361198d56e4d285b693
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-11-13 11:04:51 +0300

    Add email configurations and initialization.

--- config.py
+++ config.py
@@ -7,6 +7,15 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://access:gideon.@localhost/gidzblog'
 
 
+     #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+
+
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
     if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
